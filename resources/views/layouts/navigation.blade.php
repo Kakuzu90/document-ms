52d6128ada21159ac42b0173cadad71baec2d8e3
fdc.clyde <fdc.clyde@gmail.com>
2026-08-05 17:03:00 +0800

    fix navigation active

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -29,7 +29,10 @@
 
             {{-- Center: Nav links --}}
             <div class="flex items-center gap-1">
-                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                @php
+                    $active = request()->routeIs('admin.dashboard') || request()->routeIs('teacher.dashboard') ? true : false;
+                @endphp
+                <x-nav-link :href="route('dashboard')" :active="$active">
                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="1.5">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                     </svg>
